1ee3582edeb4d3ea310ff8cf206dbbe274c12a42
astrale <ash4567@outlook.fr>
2026-03-03 17:37:30 +0100

    adds some utilities to vec2

--- source/vector2.py
+++ source/vector2.py
@@ -1,10 +1,14 @@
 from typing import Any
+from typing_extensions import Self
 
 
 class Vector2:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
+
+    def __str__(self) -> str:
+        return f"Vector2({self.x}, {self.y})"
 
     def __add__(self, rhs: Any):
         if not isinstance(rhs, Vector2):
@@ -15,6 +19,15 @@ class Vector2:
             )
         return Vector2(self.x + rhs.x, self.y + rhs.y)
 
+    def __sub__(self, rhs: Any):
+        if not isinstance(rhs, Vector2):
+            raise ValueError(
+                "- operator not supported between Vector2 and {}".format(
+                    type(rhs)
+                )
+            )
+        return Vector2(self.x - rhs.x, self.y - rhs.y)
+
     def __eq__(self, rhs: object) -> bool:
         if not isinstance(rhs, Vector2):
             raise ValueError(
@@ -23,3 +36,11 @@ class Vector2:
                 )
             )
         return rhs.x == self.x and rhs.y == self.y
+
+    def is_in_bounds(self, bounds: Self) -> bool:
+        return (
+            self.x >= 0
+            and self.x < bounds.x
+            and self.y >= 0
+            and self.y < bounds.y
+        )
