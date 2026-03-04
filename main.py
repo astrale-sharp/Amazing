6efaa2d981023d18ec97ac3a45562b05c389bade
Paul Muller <pamuller@z2r10p8.42lyon.fr>
2026-03-04 13:57:49 +0100

    main now handle all parsing errors

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from source.walker_pa import Walker
 from source.find_way import SolveMaze
 from source.parse import Parser
 from pydantic import ValidationError
+import sys
 # from mlx import Mlx
 
 
-def main(args):
+def main():
 
     try:
-        with open("config.txt", 'r') as f:
+        with open(sys.argv[1], 'r') as f:
             args = f.read()
 
         args = Parser.parse(args)
@@ -24,8 +25,13 @@ def main(args):
 
         with open(maze.output_file, "w") as f:
             f.write(content)
-    except (ValueError, ValidationError) as e:
+    except ValidationError as e:
+        print(e)
+    except (ValueError) as e:
         print("ERROR:", e)
+    except IndexError:
+        print("ERROR: No configuration txt given as argument. \
+Please run python3 a_maze_ing <filename>.txt")
     except FileNotFoundError:
         print("Please create a config.txt with the arguments:")
         print("""    WIDTH=<int>
@@ -38,49 +44,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    maze1 = {
-        "height": 100,
-        "width": 100,
-        "entry": [49, 99],
-        "exit": [99, 99],
-        "output_file": "test.txt",
-        "perfect": True,
-    }
-
-    maze2 = {
-        "height": 6,
-        "width": 6,
-        "entry": [0, 0],
-        "exit": [5, 5],
-        "output_file": "test.txt",
-        "perfect": False,
-    }
-
-    maze3 = {
-        "height": 9,
-        "width": 9,
-        "entry": [0, 0],
-        "exit": [8, 8],
-        "output_file": "test.txt",
-        "perfect": True,
-    }
-
-    # maze4 = {
-    #         "height": 50,
-    #         "width": 50,
-    #         "entry": [0, 0],
-    #         "exit": [49, 49],
-    #         "output_file": "test.txt",
-    #         "perfect": True
-    #         }
-    # maze5 = {
-    #         "height": 50,
-    #         "width": 50,
-    #         "entry": [0, 0],
-    #         "exit": [49, 49],
-    #         "output_file": "test.txt",
-    #         "perfect": False
-    #         }
-    mazes = [maze1]
-    for maze in mazes:
-        main(maze)
+    main()
